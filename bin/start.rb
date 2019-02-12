debc144e9aee27a8c3647c319ae2655c7dc16edb
John Rees <john@bitsushi.com>
2019-02-12 12:10:13 +0000

    collate all Procfiles and then run with node foreman using yarn start

---
+++ bin/start.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+File.open("Procfile", 'w') do |file|
+  Dir["*/Procfile"].each do |procfile|
+    IO.foreach(procfile) do |line|
+      prefix = procfile.split('/').first
+
+      new_line = [prefix, line].join('-')
+
+      before, after = new_line.split(":")
+
+      file.write("#{before}: cd '#{prefix}' && #{after}")
+    end
+  end
+end
